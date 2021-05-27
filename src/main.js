292fa8353a146eba113014ee57b592cd0b2aa355
J. Andres Gutierrez <workjorge@yahoo.com>
2021-05-27 00:06:10 -0500

    lectura JSON, Sin probar

--- src/main.js
+++ src/main.js
@@ -1,35 +1,69 @@
+function imprimir_datos(datos){
+	console.info('works?');
+}
 
-			<p class="menu-button">Web front-end</p>
-			<p class="menu-button">Back-end</p>
-			<p class="menu-button">Seguridad y dev ops</p>
-			<p class="menu-button">IA y análisis de datos</p>
-			<p class="menu-button">Matemáticas</p>
-			<p class="menu-button">Programación videojuegos</p>
-			<p class="menu-button">Otros</p>
+
+function agregar_datos(jsonKey){
+	var lista = [];	
+	let datos = [];	
+
+	$.getJSON('datos.json',function (datos) {
+		var lista = [];
+		$.each(datos,function(llave, valor){
+				lista.push({
+					llave: llave,
+					valor: valor});
+			});
+		})
+		.done(function () {
+			console.info('0: OK getJSON');
+		})
+		.fail(function(){
+			console.error('0: error funcion getJSON');
+		});
+
+	for(const contenido of lista){
+		if(contenido.llave = jsonKey)
+			datos = contenido.valor
+	}
+}
 
 $(document).ready(function(){
 
-	$('.menu-button').click(()=>{
-		let eleNombre = $(this).content;
+	$('.menu-boton').click(function(){
+		let eleNombre = $(this).text();
+		let jsonKey = "";
 
 		switch(eleNombre){
 
-			case 'Web front-end':
+			case 'Front-end':
+				jsonKey = "front_end"
 				break;
 			case 'Back-end':
+				jsonKey = "back_end"
 				break;
 			case 'Seguridad y dev ops':
+				jsonKey = "server_dev_ops"
 				break;
 			case 'IA y análisis de datos':
+				jsonKey = "ia_analisis"
 				break;
 			case 'Matemáticas':
+				jsonKey = "matematicas"
 				break;
 			case 'Videojuegos':
+				jsonKey = "videojuegos"
 				break;
-			case 'Otros';
+			case 'Otros':
+				jsonKey = "otros"
 				break;
-		
+			default:
+				break;
 		}
+
+		if (json_key.length > 0){
+			agregar_datos(jsonKey);
+		}else{console.warn('0: json_key vacio');}	
 
 	});
 
